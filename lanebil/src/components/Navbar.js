3e30fd23536d6d7e190f93b1b4307658fce033ef
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2021-05-26 21:38:50 +0200

    sd

---
+++ lanebil/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavLink, withRouter } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <div className="content-page">
+            <nav className="blue">
+                <div className="nav-wrapper container">
+                <a href="/" className="brand-logo">MIN LÅNEBIL</a>
+                    <ul className="right">
+                        <li>hi</li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
